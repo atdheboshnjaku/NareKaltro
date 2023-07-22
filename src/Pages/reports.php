c80fd5ee3367b972171866a0e0d2bd3385ae5cb4
Atdhe Boshnjaku <atdhe.boshnjaku@gmail.com>
2023-07-22 20:28:44 +0200

    Add reports page and a coming soon msg to it

---
+++ src/Pages/reports.php
@@ -0,0 +1,21 @@
+<?php
+
+use Fin\Narekaltro\App\Url;
+
+require_once("../../vendor/autoload.php");
+
+require_once("Templates/header.php");
+
+?>
+
+<div class="box">
+    <div class="box-header">
+        <div class="box-lf-ctn">
+            <h2>Analytics & Reports</h2>
+            <p>Coming soon...</p>
+        </div>
+        <div class="box-rt-ctn">
+
+        </div>
+    </div>
+</div>
